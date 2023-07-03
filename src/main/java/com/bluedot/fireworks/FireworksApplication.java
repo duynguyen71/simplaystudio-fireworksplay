0b1331d3cace2799a00dc89cccfe789aca4726dd
Duy Nguyen <khanhduy.dev.bt@gmail.com>
2023-07-03 22:36:14 +0700

    first commit

---
+++ src/main/java/com/bluedot/fireworks/FireworksApplication.java
@@ -0,0 +1,13 @@
+package com.bluedot.fireworks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FireworksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FireworksApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bluedot/fireworks/FireworksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bluedot.fireworks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FireworksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
